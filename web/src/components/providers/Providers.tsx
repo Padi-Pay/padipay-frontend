226ce94a332e097fb3966d56ea10a811a035e7d5
Presser <pressergrey@gmail.com>
2026-07-28 13:42:50 +0100

    feat: add @react-oauth/google dependency and configure Providers

--- web/src/components/providers/Providers.tsx
+++ web/src/components/providers/Providers.tsx
@@ -3,17 +3,20 @@
 import React, { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 import { SessionExpiryRedirect } from '@/components/auth/SessionExpiryRedirect';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '';
+
   return (
-    <>
+    <GoogleOAuthProvider clientId={clientId}>
       <Toaster position="top-right" richColors />
       <SessionExpiryRedirect />
       {children}
-    </>
+    </GoogleOAuthProvider>
   );
 }
